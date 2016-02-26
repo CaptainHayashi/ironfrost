52a17806f8f8d631e12bd20d19465741b61ce154
Matt Windsor <mattwindsor@btinternet.com>
2016-02-26 00:22:10 +0000

    Improve and document Message.
    Message may now be used as a string enumerator.

--- libironfrost/Message.cs
+++ libironfrost/Message.cs
@@ -1,19 +1,104 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ironfrost
 {
-    public class Message
+    /// <summary>
+    ///    A Bifrost message.
+    ///    
+    ///    <para>
+    ///        <c>Message</c> can be constructed from, and used as,
+    ///        a string enumerator.
+    ///    </para>
+    /// </summary>
+    public class Message : IEnumerable<string>
     {
+        /// <summary>
+        ///     The Message's unique identifier.
+        ///     
+        ///     <para>
+        ///         Tags can also be used for routing.
+        ///     </para>
+        /// </summary>
         public string Tag { get; }
+
+        /// <summary>
+        ///     The message word, which identifies the type of message.
+        /// </summary>
         public string Word { get; }
+
+        /// <summary>
+        ///    The message arguments.
+        /// </summary>
         public string[] Args { get; }
 
-        public Message(IEnumerable<string> words)
+        /// <summary>
+        ///     Constructs a message from a tag, word, and argument list.
+        /// </summary>
+        /// <param name="tag">
+        ///     The new message's tag.
+        /// </param>
+        /// <param name="word">
+        ///     The new message's message word.
+        /// </param>
+        /// <param name="args">
+        ///     The new message's argument list.
+        /// </param>
+        public Message(string tag, string word, IEnumerable<string> args)
         {
-            Tag = words.First();
-            Word = words.Skip(1).First();
-            Args = words.Skip(2).ToArray();
+            Tag = tag;
+            Word = word;
+            Args = args.ToArray();
+        }
+
+        /// <summary>
+        ///     Constructs a message from a word list.
+        ///     
+        ///     <para>
+        ///         The first word is used as the message's tag.
+        ///         The second word is used as the message word.
+        ///         All further words are used as arguments.
+        ///     </para>
+        /// </summary>
+        /// <param name="words">
+        ///     The list of words.
+        ///     There must be at least 2 words.
+        /// </param>
+        public Message(IEnumerable<string> words)
+            : this(words.First(), words.ElementAt(1), words.Skip(2))
+        { }
+
+        /// <summary>
+        ///     Gets a string enumerator for this <c>Message</c>.
+        /// </summary>
+        /// <returns>
+        ///     A string <c>IEnumerator</c>.
+        ///     The enumerator yields the <c>Message</c>'s tag, word,
+        ///     and arguments.
+        /// </returns>
+        public IEnumerator<string> GetEnumerator()
+        {
+            yield return Tag;
+            yield return Word;
+            foreach (var arg in Args)
+            {
+                yield return arg;
+            }
+        }
+
+        /// <summary>
+        ///     Gets a string enumerator for this <c>Message</c>.
+        /// </summary>
+        /// <returns>
+        ///     A string <c>IEnumerator</c>.
+        ///     The enumerator yields the <c>Message</c>'s tag, word,
+        ///     and arguments.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
