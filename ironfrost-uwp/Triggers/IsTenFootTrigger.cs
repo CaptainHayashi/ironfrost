37865d1efdcd522f37f761033c92ba5acaf1914c
Matt Windsor <mattwindsor@btinternet.com>
2016-09-07 23:43:23 +0100

    Add beginnings of UWP attempt.
    This might get canned if I get sick of UWP, since WPF is easier to work
    with.

---
+++ ironfrost-uwp/Triggers/IsTenFootTrigger.cs
@@ -0,0 +1,37 @@
+﻿// Adapted from the Windows Universal Platform examples
+// https://github.com/Microsoft/Windows-universal-samples/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.Xaml;
+
+namespace Ironfrost.Triggers
+{
+    class IsTenFootTrigger : StateTriggerBase
+    {
+        private bool _isTenFootRequested;
+
+        public IsTenFootTrigger()
+        {
+            // Set default values.
+            IsTenFoot = true;
+        }
+
+        public bool IsTenFoot
+        {
+            get
+            {
+                return _isTenFootRequested;
+            }
+            set
+            {
+                _isTenFootRequested = value;
+                SetActive(SystemInformationHelpers.IsTenFootExperience == _isTenFootRequested);
+            }
+        }
+    }
+}
